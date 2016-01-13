ea9c3436d18068d8ac3dde1509b8f5ab47a599f5
aarontang <aarontang@tencent.com>
2016-01-13 11:51:14 +0800

    [frame]增加FrameAnimation的演示

--- app/src/main/java/com/creator/animationdemo/FrameAnimationActivity.java
+++ app/src/main/java/com/creator/animationdemo/FrameAnimationActivity.java
@@ -1,13 +1,25 @@
 package com.creator.animationdemo;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FrameAnimationActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame_animation);
+        mImageView = (ImageView) findViewById(R.id.image_view);
+    }
+
+    public void onStart(View view) {
+        mImageView.setBackgroundResource(R.drawable.frame_anim);
+        AnimationDrawable frameAnimation = (AnimationDrawable) mImageView.getBackground();
+        frameAnimation.start();
     }
 }
